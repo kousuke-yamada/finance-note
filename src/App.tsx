83e7ed1a4d4f6cd5bba8f398f30429cd2d0a0028
山田幸助 <offmailbox1225@gmail.com>
2025-06-12 23:40:30 +0900

    ・サイバーメニュー設定 ・レイアウトのデザイン設定（全体テーマ作成）

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,24 @@ import Home from "./pages/Home";
 import Report from "./pages/Report";
 import NoMatch from "./pages/NoMatch";
 import AppLayout from "./components/layout/AppLayout";
+import { theme } from "./theme/theme";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AppLayout />}>
-          <Route index element={<Home />} />
-          <Route path="/report" element={<Report />} />
-          <Route path="*" element={<NoMatch />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Home />} />
+            <Route path="/report" element={<Report />} />
+            <Route path="*" element={<NoMatch />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
