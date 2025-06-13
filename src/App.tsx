2af5310226cd164553c3f547f848080fadb2f49a
山田幸助 <offmailbox1225@gmail.com>
2025-06-13 18:32:40 +0900

    ・Firebaseの設定 ・FIreStoreへの接続・データ取得処理追加 ⇨ Homeコンポーネントへのデータ反映

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
@@ -8,15 +8,61 @@ import AppLayout from "./components/layout/AppLayout";
 import { theme } from "./theme/theme";
 import { ThemeProvider } from "@emotion/react";
 import { CssBaseline } from "@mui/material";
+import { Transaction } from "./types/index";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebase";
+import { format } from "date-fns";
+import { formatMonth } from "./utils/formatting";
 
 function App() {
+  function isFireStoreError(
+    err: unknown
+  ): err is { code: string; message: string } {
+    return typeof err === "object" && err !== null && "code" in err;
+  }
+
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+
+  useEffect(() => {
+    const fetchTransactions = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "Transactions"));
+
+        const transactionsData = querySnapshot.docs.map((doc) => {
+          return {
+            ...doc.data(),
+            id: doc.id,
+          } as Transaction;
+        });
+        setTransactions(transactionsData);
+      } catch (err) {
+        if (isFireStoreError(err)) {
+          console.error("firestoreのエラー :", err);
+          console.error("firestoreのエラーメッセージ :", err.message);
+          console.error("firestoreのエラーコード :", err.code);
+        } else {
+          console.log("一般的なエラー :", err);
+        }
+      }
+    };
+    fetchTransactions();
+  }, []);
+
+  const monthlyTransactions = transactions.filter((transaction) => {
+    return transaction.date.startsWith(formatMonth(currentMonth));
+  });
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<AppLayout />}>
-            <Route index element={<Home />} />
+            <Route
+              index
+              element={<Home monthlyTransactions={monthlyTransactions} />}
+            />
             <Route path="/report" element={<Report />} />
             <Route path="*" element={<NoMatch />} />
           </Route>
