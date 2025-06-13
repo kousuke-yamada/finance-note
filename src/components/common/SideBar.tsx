8cb62e3746ff98b4c17bbaca57423fe5bcc0a6fd
山田幸助 <offmailbox1225@gmail.com>
2025-06-13 11:29:17 +0900

    Home画面のメインコンテンツ追加 ・月別サマリー表示 ・共通レイアウト用のカラー設定

--- src/components/common/SideBar.tsx
+++ src/components/common/SideBar.tsx
@@ -78,7 +78,7 @@ const SideBar = ({
   return (
     <Box
       component="nav"
-      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+      sx={{ width: { md: drawerWidth }, flexShrink: { md: 0 } }}
       aria-label="mailbox folders"
     >
       {/* モバイル用 */}
@@ -88,7 +88,7 @@ const SideBar = ({
         onTransitionEnd={handleDrawerTransitionEnd}
         onClose={handleDrawerClose}
         sx={{
-          display: { xs: "block", sm: "none" },
+          display: { xs: "block", md: "none" },
           "& .MuiDrawer-paper": {
             boxSizing: "border-box",
             width: drawerWidth,
@@ -107,7 +107,7 @@ const SideBar = ({
       <Drawer
         variant="permanent"
         sx={{
-          display: { xs: "none", sm: "block" },
+          display: { xs: "none", md: "block" },
           "& .MuiDrawer-paper": {
             boxSizing: "border-box",
             width: drawerWidth,
