15149ceecc781558d46580dab8fc7e827693c575
山田幸助 <offmailbox1225@gmail.com>
2025-06-16 11:39:27 +0900

    ・日毎の収支表示 ・収支の内訳詳細を表示

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
 import { Box } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import MonthlySummary from "../components/MonthlySummary";
 import Calendar from "../components/Calendar";
 import TransactionMenu from "../components/TransactionMenu";
 import TransactionForm from "../components/TransactionForm";
 import { Transaction } from "../types";
+import { format } from "date-fns";
 
 interface HomeProps {
   monthlyTransactions: Transaction[];
@@ -12,6 +13,13 @@ interface HomeProps {
 }
 
 const Home = ({ monthlyTransactions, setCurrentMonth }: HomeProps) => {
+  const today = format(new Date(), "yyyy-MM-dd");
+  const [currentDay, setCurrentDay] = useState(today);
+
+  const dailyTransactions = monthlyTransactions.filter((transaction) => {
+    return transaction.date === currentDay;
+  });
+
   return (
     <Box sx={{ display: "flex" }}>
       {/* 左側コンテンツ */}
@@ -20,11 +28,17 @@ const Home = ({ monthlyTransactions, setCurrentMonth }: HomeProps) => {
         <Calendar
           monthlyTransactions={monthlyTransactions}
           setCurrentMonth={setCurrentMonth}
+          setCurrentDay={setCurrentDay}
+          currentDay={currentDay}
+          today={today}
         />
       </Box>
       {/* 右側コンテンツ */}
       <Box>
-        <TransactionMenu />
+        <TransactionMenu
+          dailyTransactions={dailyTransactions}
+          currentDay={currentDay}
+        />
         <TransactionForm />
       </Box>
     </Box>
