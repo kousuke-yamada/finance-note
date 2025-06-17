f8dc38a371dff96ac6916178e4f14228f19d6724
山田幸助 <offmailbox1225@gmail.com>
2025-06-17 23:59:55 +0900

    Reportページのレイアウト実装 日付選択エリア実装

--- src/pages/Report.tsx
+++ src/pages/Report.tsx
@@ -1,7 +1,45 @@
+import { Grid, Paper } from "@mui/material";
 import React from "react";
+import MonthSelector from "../components/MonthSelector";
+import CategoryChart from "../components/CategoryChart";
+import TransactionTable from "../components/TransactionTable";
+import BarChart from "../components/BarChart";
 
-const Report = () => {
-  return <div>Report</div>;
+interface ReportProps {
+  currentMonth: Date;
+  setCurrentMonth: React.Dispatch<React.SetStateAction<Date>>;
+}
+
+const Report = ({ currentMonth, setCurrentMonth }: ReportProps) => {
+  const commonPaperStyle = {
+    height: { xs: "auto", md: "400px" },
+    display: "flex",
+    flexDirection: "column",
+  };
+
+  return (
+    <Grid container spacing={2}>
+      <Grid size={{ xs: 12 }}>
+        <MonthSelector
+          currentMonth={currentMonth}
+          setCurrentMonth={setCurrentMonth}
+        />
+      </Grid>
+      <Grid size={{ xs: 12, md: 4 }}>
+        <Paper sx={commonPaperStyle}>
+          <CategoryChart />
+        </Paper>
+      </Grid>
+      <Grid size={{ xs: 12, md: 8 }}>
+        <Paper sx={commonPaperStyle}>
+          <BarChart />
+        </Paper>
+      </Grid>
+      <Grid size={{ xs: 12 }}>
+        <TransactionTable />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default Report;
